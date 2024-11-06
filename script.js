6af7f37a80d6d86df9cf5c7cd15f488a4e9ed716
unknown <anandrlobo@gmail.com>
2024-11-06 17:49:20 +0530

    decode and display RTA

--- script.js
+++ script.js
@@ -24,22 +24,47 @@ socket.onclose = () => {
 socket.onmessage = function(event) {
     
     received = event.data;
-    // console.log(received);
     
     receivedArray = received.split(/\r?\n/);
     receivedArray.forEach(line => {
+        line = line.replace('3:::', ''); // remove the beginning of the line
         if (line == '2::') {
             
         }
-        else if (line.search(/SETD/) > -1 || line.search(/SETS/) > -1) {
-            line = line.replace('3:::', '');    // remove the starting characters
-            document.body.insertAdjacentHTML("afterbegin", line+"<br \>");
-            // append new commands to the start of the HTML page 
-            // instead of the end so there's no need to scroll
+        else if (line.search(/SETD/) > -1 || line.search(/SETS/) > -1) {    // filter SETD and SETS messages
+            // insert messages at the top, so you don't have to scroll for new messages
+            document.getElementById("messages").insertAdjacentHTML("afterbegin", line+"<br \>");
         }
-        else if(line.startsWith("3:::RTA")){
-            line = line.replace('3:::RTA^', '');    // remove the starting characters
-            // do shit with RTA
+        else if(line.startsWith("RTA")){    // RTA decoding
+            _rta = atob(line.slice(4)); // cut off excess characters and decode Base64-encoded string
+            for(var b=[], c=0, d=_rta.length; c<d; c++){
+                var f=.004167508166392142*_rta.charCodeAt(c);   // further decoding
+                b.push(f);      // push into decoded array
+            }
+            drawCanvas(b);  // draw the RTA
         }
     });
 };
+
+canvas = document.createElement('canvas');
+canvas.width = 800;
+canvas.height = 400;
+document.getElementById("_rta").appendChild(canvas);
+const ctx = canvas.getContext('2d');
+
+function drawCanvas(arr){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+    ctx.moveTo(0, canvas.height - arr[0] * canvas.height);
+
+    gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
+    for (let i = 1; i < arr.length; i++) {
+        var _t = arr[i];
+        ctx.lineTo((i / (arr.length - 1)) * canvas.width, canvas.height - _t * canvas.height);
+        gradient.addColorStop(i/arr.length, 'hsl('+(i/arr.length * 300)+',100%,50%)');
+    }
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = gradient;
+    ctx.stroke();
+}
+
